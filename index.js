d64218a945a0e779abf953fc8acad622349b3618
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-12-10 23:57:03 -0800

    create empty 2D array in createFullGraph() to hold vertex and edge data

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const puzzleData = {
     "rows": null,
     "cols": null,
+
     "start": null,
     "finish": null,
+
+    "graph": null,
+    "cells": null,
 }
 
 const createFullGraph = (rows, cols) => {
 
     // 2D array to hold cell contents
-    let cells = Array(rows).fill(Array(cols).fill(null));
+    puzzleData["cells"] = Array(rows).fill(Array(cols).fill(null));
 
-    return null;
+    // 2D array to hold vertex and edge data
+    // the vertex at each coordinate contains info about itself and the four possible surrounding edges
+    puzzleData["graph"] = Array(rows).fill(Array(cols).fill({
+        "self": null,
+        "n": null,
+        "s": null,
+        "e": null,
+        "w": null,
+    }));
+
 }
+
+createFullGraph(3, 5);
+
+console.log(puzzleData);
