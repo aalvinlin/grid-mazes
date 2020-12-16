801d8623c42265783fea4c37b8e98c2e7c83ef4b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-12-15 17:45:15 -0800

    fix incorrect vertex count when initializing graph. Finish implementing algorithm to correctly enumerate all 8512 paths on a 4x4 grid

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puzzleData = {
     "cols": null,
 
     "start": null,
-    "finish": null,
+    "end": null,
 
     "graph": null,
     "cells": null,
@@ -17,17 +17,18 @@ const createFullGraph = (rows, cols) => {
 
     // 2D array to hold vertex and edge data
     // the vertex at each coordinate contains info about itself and the four possible surrounding edges
-    let graph = Array(rows).fill(null);
-    graph = graph.map(row => Array(cols).fill(null)); // make sure each row has a unique array
+    // there are (rows + 1) * (cols + 1) vertices
+    let graph = Array(rows + 1).fill(null);
+    graph = graph.map(row => Array(cols + 1).fill(null)); // make sure each row has a unique array
 
     // 2D array to hold visited vertex and edge data
     // the vertex at each coordinate contains info about itself and the four possible surrounding edges
     let visited = Array(rows).fill(null);
     visited = visited.map(row => Array(cols).fill(null)); // make sure each row has a unique array
 
-    for (let row = 0; row < rows; row += 1)
+    for (let row = 0; row <= rows; row += 1)
         {
-            for (let col = 0; col < cols; col += 1)
+            for (let col = 0; col <= cols; col += 1)
                 {
                     let currentLocationData = {};
 
@@ -36,7 +37,7 @@ const createFullGraph = (rows, cols) => {
                         { currentLocationData["s"] = 1; }
 
                     // add link to vertex above
-                    if (row < rows - 1)
+                    if (row < rows)
                         { currentLocationData["n"] = 1; }
 
                     // add link to vertex to the left
@@ -44,13 +45,16 @@ const createFullGraph = (rows, cols) => {
                         { currentLocationData["w"] = 1; }
 
                     // add link to vertex to the right
-                    if (col < cols - 1)
+                    if (col < cols)
                         { currentLocationData["e"] = 1; }
 
                    graph[row][col] = currentLocationData;
                 }
         }
 
+    puzzleData["rows"] = rows;
+    puzzleData["cols"] = cols;
+    
     puzzleData["cells"] = cells;
     puzzleData["graph"] = graph;
     puzzleData["visited"] = visited;
@@ -64,20 +68,90 @@ const setStartAndEnd = (startX, startY, endX, endY) => {
 
 const findAllSolutions = () => {
 
-    // use a 1D array of length (rows * cols) to keep track of whether vertices are available to use
-    let availableVertices = Array(puzzleData["rows"] * puzzleData["cols"]).fill(true);
+    let [startX, startY] = puzzleData["start"];
+    let [endX, endY] = puzzleData["end"];
 
-    // mark (0, 0), the starting point, as unavailable
-    availableVertices[0] = false;
+    let verticesPerRow = puzzleData["rows"] + 1;
+    let verticesPerCol = puzzleData["cols"] + 1;
+
+    // use a 1D array of length (verticesPerRow * verticesPerCol) to keep track of whether vertices are available to use
+    let availableVertices = Array((verticesPerRow * verticesPerCol)).fill(true);
+
+    // mark the starting point, as unavailable
+    availableVertices[startY * verticesPerRow + startX] = false;
+
+    // store solutions in a set
+    allSolutions = new Set();
 
     const findAllSolutionsHelper = (currentX, currentY, pathSoFar, remainingVertices) => {
+        
+        // make a copy of remaining vertices to update for the next round
+        remainingVertices = [...remainingVertices];
+
+        // mark the current vertex as visited
+        remainingVertices[currentY * verticesPerRow + currentX] = false;
+
+        for (let direction in puzzleData["graph"][currentY][currentX]) // row first, then column
+            {
+                // if the vertex to the north is available...
+                if (direction === "n" && remainingVertices[(currentY + 1) * verticesPerRow + currentX])
+                    {
+                        // if target reached, add solution to set of known solutions
+                        if (currentX === endX && currentY + 1 === endY)
+                            { allSolutions.add(pathSoFar + "n"); }
+
+                        // continue finding any (longer) solutions from this coordinate
+                        findAllSolutionsHelper(currentX, currentY + 1, pathSoFar + "n", remainingVertices);
+                    }
+
+                // if the vertex to the south is available...
+                else if (direction === "s" && remainingVertices[(currentY - 1) * verticesPerRow + currentX])
+                    {
+                        // if target reached, add solution to set of known solutions
+                        if (currentX === endX && currentY - 1 === endY)
+                            { allSolutions.add(pathSoFar + "s"); }
+
+                        // continue finding any (longer) solutions from this coordinate
+                        findAllSolutionsHelper(currentX, currentY - 1, pathSoFar + "s", remainingVertices);
+                    }
+
+                // if the vertex to the east is available...
+                else if (direction === "e" && remainingVertices[currentY * verticesPerRow + (currentX + 1)])
+                    {
+                        // if target reached, add solution to set of known solutions
+                        if (currentX + 1 === endX && currentY === endY)
+                            { allSolutions.add(pathSoFar + "e"); }
+
+                        // continue finding any (longer) solutions from this coordinate
+                        findAllSolutionsHelper(currentX + 1, currentY, pathSoFar + "e", remainingVertices);
+                    }
+
+                // if the vertex to the west is available...
+                else if (direction === "w" && remainingVertices[currentY * verticesPerRow + (currentX - 1)])
+                    {
+                        // if target reached, add solution to set of known solutions
+                        if (currentX - 1 === endX && currentY === endY)
+                            { allSolutions.add(pathSoFar + "w"); }
+
+                        // continue finding any (longer) solutions from this coordinate
+                        findAllSolutionsHelper(currentX - 1, currentY, pathSoFar + "w", remainingVertices);
+                    }
+
+            }
 
     }
 
-    return findAllSolutionsHelper(0, 0, "", [...availableVertices]);
+    findAllSolutionsHelper(0, 0, "", [...availableVertices]);
+
+    return allSolutions;
 
 }
 
-createFullGraph(3, 5);
+createFullGraph(4, 4);
+setStartAndEnd(0, 0, 4, 4);
 
-console.log(puzzleData);
+// console.log(puzzleData["graph"])
+
+let allPaths = findAllSolutions();
+
+console.log(allPaths);
